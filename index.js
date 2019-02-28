23e0cc166dbb4aa383891bc684d7c6406f385a7e
selamawit Ayalew <slmwt_assefa@yahoo.com>
2019-02-28 08:53:35 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+//mkdir
+//npm init
+//npm install express ejs request --save
+//touch index.js
+
+var express = require("express");
+var app =express();
+var request = require('request');
+
+
+app.set('view engine', 'ejs' );
+app.get("/", function(req, res){
+    res.render("search");
+});
+
+app.get("/results", function(req, res) {
+   var query = req.query.search.trim();
+   var url = 'http://www.omdbapi.com/?&s=' + query + '&apikey=thewdb';
+    request(url, function(error, response, body){
+        if(!error && response.statusCode ==200) {
+           var data = JSON.parse(body);
+           res.render("results", {data: data});
+          
+        }
+    });
+} );
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("SERVER IS STRARTING...");
+});
